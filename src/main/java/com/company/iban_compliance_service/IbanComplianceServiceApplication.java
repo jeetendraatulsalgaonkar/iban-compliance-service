cd1e56e3e48d173fb4bf6625429452b46e5d1e44
Jeetendra Salgaonkar <jeetendra-atul.salgaonkar@sixt.com>
2025-03-07 13:58:29 +0100

    first commit

---
+++ src/main/java/com/company/iban_compliance_service/IbanComplianceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.iban_compliance_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbanComplianceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbanComplianceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/iban_compliance_service/IbanComplianceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.iban_compliance_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbanComplianceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
